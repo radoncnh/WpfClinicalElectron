fa15ba3c88d0329d057274b4ba62cf2bc8ff0fd1
Nathan Nelson <Nathan.C.Nelson@hitchcock.org>
2021-10-05 17:32:26 -0400

    Add project files.

---
+++ WpfClinicalElectron/MainControl.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace WpfClinicalElectron
+{
+    /// <summary>
+    /// Interaction logic for MainControl.xaml
+    /// </summary>
+    public partial class MainControl : UserControl
+    {
+        public MainControl()
+        {
+            InitializeComponent();
+        }
+
+        public Patient patient;
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string circle_diameter = circleTextBox.Text;
+            string selected_machine = ((ComboBoxItem)machineComboBox.SelectedItem).Content.ToString();
+            string selected_energy = ((ComboBoxItem)energyComboBox.SelectedItem).Content.ToString();
+            string selected_gantry_angle = gantryangleTextBox.Text;
+            string selected_collimator_angle = collimatorangleTextBox.Text;
+            string selected_couch_angle = couchangleTextBox.Text;
+            string selected_SSD = ssdTextBox.Text;
+            string selected_dose = doseTextBox.Text;
+            string selected_fractions = fractionsTextBox.Text;
+
+            int phantom_slice_num;
+            int fractions;
+
+            double phantom_radius_mm = 150.0;
+            double phantom_diameter_cm = 30.0;
+
+            double dose_per_fx;
+
+            if (!Double.TryParse(selected_dose, out dose_per_fx))
+            {
+                dose_per_fx = 180.0;
+            }
+
+            if (!int.TryParse(selected_fractions, out fractions))
+            {
+                fractions = 1;
+            }
+
+            if (!Double.TryParse(circle_diameter, out phantom_diameter_cm))
+            {
+                phantom_diameter_cm = 30.0;
+            }
+
+
+            if (phantom_diameter_cm < 10.0)
+                phantom_diameter_cm = 10.0;
+            if (phantom_diameter_cm > 50.0)
+                phantom_diameter_cm = 50.0;
+
+            phantom_radius_mm = (phantom_diameter_cm / 2.0) * 10.0;
+
+            phantom_slice_num = Convert.ToInt32((phantom_diameter_cm * 10.0) / 2.5);
+            phantom_slice_num += 1;  // Force phantom_slice_num to be odd
+            
+
+            StructureSet ph = patient.AddEmptyPhantom("ElectronPhantom", PatientOrientation.HeadFirstSupine, 512, 512, 600, 600, phantom_slice_num, 2.5);
+
+            Structure body = ph.AddStructure("EXTERNAL", "BODY");
+
+
+            // sphere creation
+            int center_slice = (phantom_slice_num - 1) / 2;
+            int slice_mirror = center_slice - 1;
+            double radius = phantom_radius_mm;
+            double x_pt;
+
+            for (int i = center_slice; i < phantom_slice_num; i++)
+            {
+                x_pt = 2.5 * (i - center_slice);
+                if (x_pt < phantom_radius_mm)
+                {
+                    radius = Math.Sqrt((phantom_radius_mm * phantom_radius_mm) - (x_pt * x_pt));
+                    if (i == center_slice)
+                    {
+                        draw_circle_on_plane(body, i, radius);
+                    }
+                    else
+                    {
+                        draw_circle_on_plane(body, i, radius);
+                        draw_circle_on_plane(body, slice_mirror, radius);
+                        slice_mirror -= 1;
+                    }
+                }
+            }
+                
+
+            Course course = patient.AddCourse();
+            course.Id = "Clinical Setup";
+
+            ExternalPlanSetup electron_plan = course.AddExternalPlanSetup(ph);
+            electron_plan.SetPrescription(fractions, new DoseValue(dose_per_fx, DoseValue.DoseUnit.cGy), 1.0);
+
+            ExternalBeamMachineParameters machine_parameters = new ExternalBeamMachineParameters(selected_machine, selected_energy, 600, "STATIC", null);
+            VRect<double> jawPositions = new VRect<double>(-100, -100, 100, 100);
+            double gantryAngle = Convert.ToDouble(selected_gantry_angle);
+            double collimatorAngle = Convert.ToDouble(selected_collimator_angle);
+            double couchAngle = Convert.ToDouble(selected_couch_angle);
+
+
+            VVector iso_body = body.CenterPoint;
+            VVector isocenter = new VVector(iso_body.x, iso_body.y, iso_body.z);
+            ph.Image.UserOrigin = isocenter;
+
+            double isocenter_radius_mm;
+            double SSD = 100.0;
+
+            SSD = Convert.ToDouble(selected_SSD);
+            if (SSD < 90.0)
+                SSD = 90.0;
+            if (SSD > 110.0)
+                SSD = 110.0;
+
+            isocenter_radius_mm = phantom_radius_mm + ((SSD - 100.0)) * 10.0;
+            isocenter.x = isocenter_radius_mm * Math.Sin(degreesToradians(gantryAngle));
+            isocenter.y = (isocenter_radius_mm * Math.Cos(degreesToradians(gantryAngle)))*-1.0;
+
+            Beam beam = electron_plan.AddStaticBeam(machine_parameters, jawPositions, collimatorAngle, gantryAngle, couchAngle, isocenter);
+
+            MessageBox.Show("Done!");
+
+        }
+
+        double degreesToradians(double degrees)
+        {
+            double radians;
+
+            radians = (Math.PI * degrees) / 180.0;
+
+            return radians;
+        }
+
+        void draw_circle_on_plane(Structure Body, int slice, double circle_radius_mm)
+        {
+            int num_theta = 50;
+            double x, y, theta = 0.0;
+            double dtheta = 2.0 * Math.PI / (double)num_theta;
+            VVector[] circle_coords = new VVector[num_theta];
+
+            for (int i = 0; i < num_theta; i++)
+            {
+                x = circle_radius_mm * Math.Cos(theta);
+                y = circle_radius_mm * Math.Sin(theta);
+
+                //VVectors to add distances are in mm
+
+                circle_coords[i] = new VVector(x, y, slice);
+                theta += dtheta;
+            }
+
+            Body.AddContourOnImagePlane(circle_coords, slice);
+            Body.SetAssignedHU(0.0);
+        }
+    }
+}
---
+++ WpfClinicalElectron/Script.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using WpfClinicalElectron;
+
+[assembly: ESAPIScript(IsWriteable =true)]
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context, Window Mainwindow)
+        {
+            context.Patient.BeginModifications();
+
+            // Add existing WPF control to the script window.
+            MainControl mainControl = new MainControl();
+           
+            Mainwindow.Content = mainControl;
+            Mainwindow.SizeToContent = SizeToContent.WidthAndHeight;
+            Mainwindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Mainwindow.Title = "Clinical Electron Setup";
+
+            mainControl.patient = context.Patient;
+        }
+    }
+}
